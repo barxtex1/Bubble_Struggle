c5c350b44738d40eae153ddef54869f244aa4807
barxtex <bartekpopielarczyk@wp.pl>
2020-05-30 13:49:45 +0200

    inicjalizacja enemy

--- Bubble_Strugle/main.cpp
+++ Bubble_Strugle/main.cpp
@@ -4,7 +4,8 @@ int main()
 {
     Scena s(1000,800);
     Player hero(s.getWidth(),s.getHeight());
-    Enemy* ball;
+    Enemy* ball = nullptr;
     s.loop(hero,ball);
+    delete ball;
     return 0;
 }
